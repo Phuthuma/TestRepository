81412eccbb174c0a08dea56ecf426e9eddc8694e
Phuthuma Petse <phuthumaloyisopetse@gmail.com>
2018-06-15 12:16:54 +0200

    HelloWordApp
    We are just telling ourselves how much effort we will put into this project.

---
+++ HelloAgain/Task01/src/nmmu/projHons/Main.java
@@ -0,0 +1,9 @@
+package nmmu.projHons;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("I will give my best to this project!");
+        System.out.println("I will dedicate myself, so that i don't get caught off-guard!");
+        System.out.println("I am tired of loosing!");
+    }
+}
